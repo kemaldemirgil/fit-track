f885a0ab4feb9bb913e0d6352746a12dbc36f298
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-18 11:26:19 -0400

    deploying

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fittrackdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-// routes
-app.use(require("./routes/api.js"));
+
+// // routes
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
