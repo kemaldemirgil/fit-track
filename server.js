a67ce1b3fb176b86845743adcd846b16d8eb7e8c
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-18 11:45:13 -0400

    adding structure

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(
@@ -21,8 +23,9 @@ mongoose.connect(
 );
 
 
-// // routes
-// app.use(require("./routes/api.js"));
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
